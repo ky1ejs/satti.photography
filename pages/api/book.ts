7f7a07419dd9ad183090fcbdd4a0fff8dff8e9ab
Kyle Satti <k@kylejs.dev>
2023-01-10 21:02:21 -0500

    Remove debugging logs

--- pages/api/book.ts
+++ pages/api/book.ts
@@ -12,7 +12,6 @@ export default function handler(
   res: NextApiResponse<void>
 ) {
   const customer: Customer = req.body;
-  console.log(customer);
   return addCustomerToNotion(customer)
     .then(() =>
       Promise.all([
@@ -30,7 +29,6 @@ export default function handler(
           : Promise.resolve(),
       ])
     )
-    .then(() => console.log("emails sent"))
     .then(() => res.status(200).send())
     .catch(() => res.status(500).send());
 }
