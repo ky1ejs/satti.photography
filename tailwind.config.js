d61f97376509664567daaf2365435e8cc85a32ae
Kyle Satti <k@kylejs.dev>
2023-03-05 14:48:55 -0500

    Set custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      base: ["Nobel-Book", "sans-serif"],
+    },
     extend: {
       colors: {
         "gray-dark": "#202020",
