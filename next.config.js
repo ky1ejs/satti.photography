c7e308716b1c121965d2d169012a417f92531442
Kyle Satti <k@kylejs.dev>
2023-03-29 14:17:51 -0400

    Finish sentry setup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const moduleExports = {
   reactStrictMode: true,
   async redirects() {
     return [
@@ -10,6 +17,16 @@ const nextConfig = {
       },
     ];
   },
+  sentry: {
+    tunnelRoute: "/monitoring",
+  },
 };
 
-module.exports = nextConfig;
+const SentryWebpackPluginOptions = {
+  silent: false,
+  dryRun: false,
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,6 @@ import type { AppProps } from "next/app";
 import React from "react";
 import { Page } from "../components/Page";
 import { FormContextProvider } from "../contexts/FormContext";
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
-
-Sentry.init({
-  dsn: "https://41d42f09dd3e4feda0eba4ee2063b892@o4504922846789632.ingest.sentry.io/4504922854522880",
-  integrations: [new BrowserTracing(), new Sentry.Replay()],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
