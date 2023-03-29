d47d02d661e7d6e8d82cfd725f42993601a34ebe
Kyle Satti <k@kylejs.dev>
2023-03-29 15:32:13 -0400

    Change sentry configs to be js not ts

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,3 +1,7 @@
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
@@ -6,7 +10,7 @@ Sentry.init({
   dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
-  tunnel: "/api/tunnel",
+  tunnel: "/api/monitoring",
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
