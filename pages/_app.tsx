6bc54be75515e352e2aaa3e6f0c68e2bda55051f
Kyle Satti <k@kylejs.dev>
2023-03-29 14:46:04 -0400

    Adds vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,17 @@ import type { AppProps } from "next/app";
 import React from "react";
 import { Page } from "../components/Page";
 import { FormContextProvider } from "../contexts/FormContext";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <FormContextProvider>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
-    </FormContextProvider>
+    <>
+      <FormContextProvider>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </FormContextProvider>
+      <Analytics />
+    </>
   );
 }
