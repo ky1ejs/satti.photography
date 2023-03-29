eb3c198d7dfbc4f286e2ccb1fa821adeac944154
Kyle Satti <k@kylejs.dev>
2023-03-29 14:33:33 -0400

    Fix sentry init

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -2,7 +2,7 @@
 // The config you add here will be used whenever middleware or an Edge route handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
